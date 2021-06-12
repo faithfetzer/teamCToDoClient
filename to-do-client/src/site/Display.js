12fd19bcdb3db4d70d87278c60e13af90e36db31
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-06-12 10:45:31 -0400

    imported google fonts

--- to-do-client/src/site/Display.js
+++ to-do-client/src/site/Display.js
@@ -1,3 +1,16 @@
 // main body of page- las list view/create/edit/delete displayed
 // when logged out, shows welcome/landing OR login/signup if user has selected that already,
 // when logged in has list view/create/edit/delete displayed
+
+import React from 'react';
+
+const Display = () => {
+
+    return(
+        <div>
+            Display Body
+        </div>
+    )
+}
+
+export default Display;
