318a8c49eeeec22dc47df5b94b891268e953d6ad
Hopeacuna <esperanzahopeacuna@gmail.com>
2021-06-16 06:46:40 -0400

    minor edit

--- to-do-client/src/components/Lists/EditListItem/EditListItem.js
+++ to-do-client/src/components/Lists/EditListItem/EditListItem.js
@@ -1,56 +1,56 @@
-import React, { useState } from 'react';
-import APIURL from '../../../helpers/environment';
+// import React, { useState } from 'react';
+// import APIURL from '../../../helpers/environment';
 
 
 
 
-const EditListItem = (props) => {
-    console.log(props)
-    //console.log(props.list.itemToEdit);
-    const [editName, setEditName] = useState(props.entryToEdit.name);
-    const [editDate, setEditDate] = useState(props.entryToEdit.date);
-    const [editTimeDue, setEditTimeDue] = useState(props.entryToEdit.timedue);
-    const [editDescription, setEditDescription] = useState(props.entryToEdit.description);
-    const [editDuration, setEditDuration] = useState(props.entryToEdit.duration);
-    const [editCompleted, setEditCompleted] = useState(props.entryToEdit.completed);
-    const [editImportant, setEditImportant] = useState(props.entryToEdit.important);
-    const listUpdate = (event) => {
-        event.preventDefault();
-        fetch(`${APIURL}/list/${props.itemToEdit}`, {
-            method: 'PUT',
-            body: JSON.stringify({list: {name: editName, date: editDate, timedue: editTimeDue, description: editDescription, duration: editDuration}}),
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                'Authorization': props.token
-            })
-        }) .then((res => {
-            //props.setList([]);
-            props.setItemToEdit(undefined);
-        }))
-    }
+// const EditListItem = (props) => {
+//     console.log(props)
+//     //console.log(props.list.itemToEdit);
+//     const [editName, setEditName] = useState(props.entryToEdit.name);
+//     const [editDate, setEditDate] = useState(props.entryToEdit.date);
+//     const [editTimeDue, setEditTimeDue] = useState(props.entryToEdit.timedue);
+//     const [editDescription, setEditDescription] = useState(props.entryToEdit.description);
+//     const [editDuration, setEditDuration] = useState(props.entryToEdit.duration);
+//     const [editCompleted, setEditCompleted] = useState(props.entryToEdit.completed);
+//     const [editImportant, setEditImportant] = useState(props.entryToEdit.important);
+//     const listUpdate = (event) => {
+//         event.preventDefault();
+//         fetch(`${APIURL}/list/${props.itemToEdit}`, {
+//             method: 'PUT',
+//             body: JSON.stringify({list: {name: editName, date: editDate, timedue: editTimeDue, description: editDescription, duration: editDuration}}),
+//             headers: new Headers({
+//                 'Content-Type': 'application/json',
+//                 'Authorization': props.token
+//             })
+//         }) .then((res => {
+//             //props.setList([]);
+//             props.setItemToEdit(undefined);
+//         }))
+//     }
 
 
-    return (
-        //add some sort of pop up here
-        <div>
-            Edit List Item
-            <button onClick={() => { props.setItemToEdit(undefined) }}>Cancel</button>
-            <button>Submit</button>
-            {/* <div className='header'>
-                Edit To-Do List
-            </div>
-            <div className='body'>
-                table with all params w/ input fields 
-            </div>
-            <div className='footer'>
-                Save edit button
-            </div> */}
+//     return (
+//         //add some sort of pop up here
+//         <div>
+//         //     Edit List Item
+//         //     <button onClick={() => { props.setItemToEdit(undefined) }}>Cancel</button>
+//         //     <button>Submit</button>
+//             <div className='header'>
+//                 Edit To-Do List
+//             </div>
+//             <div className='body'>
+//                 table with all params w/ input fields 
+//             </div>
+//             <div className='footer'>
+//                 Save edit button
+//             </div>
 
-        </div>
-    )
+//         </div>
+//     )
 
-}
+// }
 
 
-export default EditListItem;
-// goes to /list/update endpoint
+// export default EditListItem;
+// // goes to /list/update endpoint
